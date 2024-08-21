e6e417710df1f91e0485e7bfd0bb1c122df275a1
Marius <marius.havnaas@drift.oslo.kommune.no>
2024-08-21 16:20:30 +0200

    init

---
+++ CodeAlongNetCoreWebApiNet6/Models/Owner.cs
@@ -0,0 +1,6 @@
+namespace CodeAlongNetCoreWebApiNet6.Models;
+
+public class Owner
+{
+    
+}
---
+++ CodeAlongNetCoreWebApiNet6/Models/Pokemon.cs
@@ -0,0 +1,8 @@
+namespace CodeAlongNetCoreWebApiNet6.Models;
+
+public class Pokemon
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime BirthDate { get; set; }
+}
