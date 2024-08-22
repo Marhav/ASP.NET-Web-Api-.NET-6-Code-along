b6e4827c807735727fbafb35037081d9a3162608
Marius <marius.havnaas@drift.oslo.kommune.no>
2024-08-22 10:29:55 +0200

    Satt opp database med Entity Framework.

--- CodeAlongNetCoreWebApiNet6/Program.cs
+++ CodeAlongNetCoreWebApiNet6/Program.cs
@@ -1,11 +1,34 @@
+using CodeAlongNetCoreWebApiNet6;
+using CodeAlongNetCoreWebApiNet6.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<Seed>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
+
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
+    SeedData(app);
+
+void SeedData(IHost app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<Seed>();
+        service.SeedDataContext();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
